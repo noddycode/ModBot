f18bf66b507892176a9d469ff9ea72093cd7e3d7
noddycode <neorescuer@gmail.com>
2017-02-22 15:39:30 -0500

    Started chaing update function

--- updateHandler.js
+++ updateHandler.js
@@ -5,139 +5,169 @@ var rp = require('request-promise');
 var cheerio = require('cheerio');
 var Config = require(process.argv[2]);
 var Util = require("./util.js");
+var Promise = require('bluebird');
+var Tumblr = require('tumblr.js');
 
 var bot = new Discord.Client();
 var updateChannel;
 var lastPost;
 var refresh = parseInt(Config.update.refresh);
 
-//Gets the latest post id when the bot is started
-var getInitialPost = function()
+var ModBot = ModBot || {};
+
+ModBot.UpdateHandler = class
 {
-	return rp(
-			//Options here request the iphone6 version of the page to give us a standard theme to reliably scrape
-			//Note that any participating blogs MUST have the default mobile view enabled for this to work
-			{
-				uri: Config.update.url,
-				headers: {
-					'User-Agent': 'Apple-iPhone7C2/1202.466'
-				}
-			}
-		)
-		.then(function(html)
-		{
-			var $ = cheerio.load(html);
-			let post = getPostList($)[0];
-			return getID(post.url);
-		})
-		.catch(function(err)
-		{
-			console.log("Could not reach webpage, exiting...");
-			process.exit();
-		});
-}
-
-function getPostList($)
-{
-	var json = $("[type = 'application/ld+json']", "head").text();
-
-	var posts = JSON.parse(json).itemListElement;
-
-	return posts;
-}
-
-function getID(url)
-{
-	let res = url.split('/');
-	return res[4];
-}
-
-var update = function ()
-{
-	rp(	
-			//Options here request the iphone6 version of the page to give us a standard theme to reliably scrape
-			//Note that any participating blogs MUST have the default mobile view enabled for this to work
-			{
-				uri: Config.update.url,
-				headers: {
-					'User-Agent': 'Apple-iPhone7C2/1202.466'
-				}
-			}
-		)
-	.then( html =>
+	constructor(key, secret, bot)
 	{
-		var $ = cheerio.load(html);
-		let posts = getPostList($);
-		if (posts.length <= 0)
-			return;
-
-		let postStack = []; //Stack is used in case there are multiple posts between refreshes
-
-		posts.some(function(p) 
+		let authObject = 
 		{
-			// Breaks once function is caught up to last update
-			if (getID(p.url) === lastPost)
+			credentials:
 			{
-				return true;
-			}
-
-			//get the post id for further processing
-			let id = getID(p.url);
-
-			let tags = [];
-			$(".tag-link", `.post-${id}`).each(function() 
-				{
-					tags.push($(this).text());
-				});
-
-			if (tags.length <= 0)
-				return false;
-
-			let comp = Util.inArray(tags, Config.update.tags);
-			
-			if(comp)
-			{
-				postStack.push(p.url);
-			}
-
-			return false;
-
-		});
-
-		if (postStack.length <= 0)
-			return;
-
-		lastPost = getID(postStack[0]);
-		while (postStack.length > 0)
-		{
-			updateChannel.sendMessage(`**Update**: ${postStack.pop()}`);
-			console.log("Blog updated");
+				consumer_key: key,
+				consumer_secret: secret
+			},
+			returnPromises: true
 		}
 
+		console.log(bot);
+
+		this.client = new Tumblr.Client(authObject);
+
+		this.client.blogPosts('noddybots', {limit:6, tag: "mb, pony liveblogs"}).then(resp =>
+		{
+			console.log(resp.posts);
+		});
+	}
+}
+
+// //Gets the latest post id when the bot is started
+// var getInitialPost = function()
+// {
+// 	return rp(
+// 			//Options here request the iphone6 version of the page to give us a standard theme to reliably scrape
+// 			//Note that any participating blogs MUST have the default mobile view enabled for this to work
+// 			{
+// 				uri: Config.update.url,
+// 				headers: {
+// 					'User-Agent': 'Apple-iPhone7C2/1202.466'
+// 				}
+// 			}
+// 		)
+// 		.then(function(html)
+// 		{
+// 			var $ = cheerio.load(html);
+// 			let post = getPostList($)[0];
+// 			return getID(post.url);
+// 		})
+// 		.catch(function(err)
+// 		{
+// 			console.log("Could not reach webpage, exiting...");
+// 			process.exit();
+// 		});
+// }
+
+// function getPostList($)
+// {
+// 	var json = $("[type = 'application/ld+json']", "head").text();
+
+// 	var posts = JSON.parse(json).itemListElement;
+
+// 	return posts;
+// }
+
+// function getID(url)
+// {
+// 	let res = url.split('/');
+// 	return res[4];
+// }
+
+// var update = function ()
+// {
+// 	rp(	
+// 			//Options here request the iphone6 version of the page to give us a standard theme to reliably scrape
+// 			//Note that any participating blogs MUST have the default mobile view enabled for this to work
+// 			{
+// 				uri: Config.update.url,
+// 				headers: {
+// 					'User-Agent': 'Apple-iPhone7C2/1202.466'
+// 				}
+// 			}
+// 		)
+// 	.then( html =>
+// 	{
+// 		var $ = cheerio.load(html);
+// 		let posts = getPostList($);
+// 		if (posts.length <= 0)
+// 			return;
+
+// 		let postStack = []; //Stack is used in case there are multiple posts between refreshes
+
+// 		posts.some(function(p) 
+// 		{
+// 			// Breaks once function is caught up to last update
+// 			if (getID(p.url) === lastPost)
+// 			{
+// 				return true;
+// 			}
+
+// 			//get the post id for further processing
+// 			let id = getID(p.url);
+
+// 			let tags = [];
+// 			$(".tag-link", `.post-${id}`).each(function() 
+// 				{
+// 					tags.push($(this).text());
+// 				});
+
+// 			if (tags.length <= 0)
+// 				return false;
+
+// 			let comp = Util.inArray(tags, Config.update.tags);
+			
+// 			if(comp)
+// 			{
+// 				postStack.push(p.url);
+// 			}
+
+// 			return false;
+
+// 		});
+
+// 		if (postStack.length <= 0)
+// 			return;
+
+// 		lastPost = getID(postStack[0]);
+// 		while (postStack.length > 0)
+// 		{
+// 			updateChannel.sendMessage(`**Update**: ${postStack.pop()}`);
+// 			console.log("Blog updated");
+// 		}
+
 		
-	})
-	.catch(function(err)
-	{
-		console.log(`ERROR: Could not get webpage: '${err}'`);
-	});
-}
+// 	})
+// 	.catch(function(err)
+// 	{
+// 		console.log(`ERROR: Could not get webpage: '${err}'`);
+// 	});
+// }
 
-var getNewPosts = function()
-{
-	update();
-	setInterval(update, refresh);
-}
+// var getNewPosts = function()
+// {
+// 	update();
+// 	setInterval(update, refresh);
+// }
 
-var handleReq = function(){request(Config.update.url, getNewPosts)};
+// var handleReq = function(){request(Config.update.url, getNewPosts)};
 
 bot.login(Config.token)
 
 bot.on('ready', () => {
-  console.log('Updater process ready!');
-  updateChannel = bot.channels.get(Config.ids.updatech);
-  getInitialPost().then(val => 
-  	{
-  		lastPost = val;
-  		getNewPosts()
-  	});
+	var udh = new ModBot.UpdateHandler(Config.update.tumblr_token, Config.update.tumblr_secret, this);
+	console.log('Updater process ready!');
+  // updateChannel = bot.channels.get(Config.ids.updatech);
+  // getInitialPost().then(val => 
+  // 	{
+  // 		lastPost = val;
+  // 		getNewPosts()
+  // 	});
 });
